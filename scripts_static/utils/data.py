1b78e84e413c168f919443444c6259170a40a338
Sungjun Cho <sungjun.cho@psych.ox.ac.uk>
2023-09-21 15:00:52 +0100

    add: load site and scanner info

--- scripts_static/utils/data.py
+++ scripts_static/utils/data.py
@@ -3,6 +3,7 @@
 """
 
 import os
+import numpy as np
 from glob import glob
 
 def get_subject_ids(data_dir, modality):
@@ -45,7 +46,7 @@ def get_subject_ids(data_dir, modality):
 
 def load_group_information(subject_ids):
     """Get subject indices in each group (amyloid negative controls
-       vs. amyloid positive patients with MCI or AD)
+       vs. amyloid positive patients with MCI or AD).
 
     Parameters
     ----------
@@ -69,3 +70,80 @@ def load_group_information(subject_ids):
             ap_idx.append(i)
 
     return an_idx, ap_idx
+
+def load_site_information(subject_ids):
+    """Get recording sites for each subject. The site should be 
+       either Oxford or Cambridge.
+
+    Parameters
+    ----------
+    subject_ids : list of str
+        List of subject IDs.
+
+    Returns
+    -------
+    sites : np.ndarray
+        Array marking recording sites for each subject.
+        Cambridge is marked by 0, and Oxford is marked by 1.
+    """
+
+    sites = np.zeros(len(subject_ids),)
+    for n, id in enumerate(subject_ids):
+        if int(id[1]) in [1, 3]: # site: Cambridge
+            sites[n] = 0
+        if int(id[1]) == 2: # site: Oxford
+            sites[n] = 1
+    
+    return sites
+
+def load_scanner_information(subject_ids, df, modality="meg"):
+    """Get measurement devices for each subject.
+       Available MEG scanners:
+            - Electra VectorView system (0)
+            - MEGIN TRIUX Neo system (1)
+       Available EEG scanners:
+            - 70-channel EasyCap (0)
+            - 64-channel EasyCap (1)
+            - 60-channel EasyCap (2)
+
+    Parameters
+    ----------
+    subject_ids : list of str
+        List of subject IDs.
+    df : pandas.DataFrame
+        Dataframe containing scanner information.
+    modality : str
+        Type of a neuroimaging modality.
+
+    Returns
+    -------
+    scanners : np.ndarray
+        Array of scanner devices for each subject.
+    """
+
+    # Validation
+    if modality not in ["meg", "eeg"]:
+        raise ValueError("modality should be either MEG or EEG.")
+
+    # Get scanner information
+    scanners = np.zeros(len(subject_ids),)
+    for n, id in enumerate(subject_ids):
+        device_name = df[f"{modality}_device"][df["ID"] == id].values[0]
+        if modality == "meg":
+            if device_name == "VectorView":
+                scanners[n] = 0
+            elif device_name == "TRIUX":
+                scanners[n] = 1
+            else:
+                raise ValueError(f"unexpected scanner device reported for {id}.")
+        if modality == "eeg":
+            if device_name == "EasyCap70":
+                scanners[n] = 0
+            elif device_name == "EasyCap64":
+                scanners[n] = 1
+            elif device_name == "EasyCap60":
+                scanners[n] = 2
+            else:
+                raise ValueError(f"unexpected scanner device reported for {id}.")
+    
+    return scanners
