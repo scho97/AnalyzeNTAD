22768934e57a498ce541fd87931eac7943c6b976
Sungjun Cho <sungjun.cho@psych.ox.ac.uk>
2023-09-21 18:48:20 +0100

    add: functions for GLM modeling and max-t permutation test

--- scripts_static/utils/statistics.py
+++ scripts_static/utils/statistics.py
@@ -5,7 +5,13 @@
 import warnings
 import mne
 import numpy as np
+import pandas as pd
+import glmtools as glm
 from scipy import stats
+from utils.data import (get_subject_ids,
+                        load_group_information,
+                        load_site_information,
+                        load_scanner_information)
 
 def cluster_perm_test(x1, x2, bonferroni_ntest=None):
     """Wrapper for mne.stats.permutation_cluster_test.
@@ -177,3 +183,170 @@ def stat_ind_two_samples(samples1, samples2, bonferroni_ntest=None, test=None):
     print(f"[Bonferroni Correction] p-value={pval}")
 
     return stat, pval
+
+def fit_glm(
+        input_data,
+        modality,
+        dimension_labels=None,
+        plot_verbose=False,
+        save_path=""
+    ):
+    """Fit a General Linear Model (GLM) to an input data given a design matrix.
+
+    Parameters
+    ----------
+    input_data : np.ndarray
+        Data to fit. Shape must be (n_subjects, n_features1, n_features2, ...).
+    modality : str
+        Type of neuroimaging modality.
+    dimension_labels : list of str
+        Labels for the dimensions of an input data. Defaults to None, in which 
+        case the labels will set as ["Subjects", "Features1", "Features2", ...].
+    plot_verbose : bool
+        Whether to plot the deisign matrix. Defaults to False.
+    save_path : str
+        File path to save the design matrix plot. Relevant only when plot_verbose 
+        is set to True.
+        
+
+    Returns
+    -------
+    model : glmtools.fit.OLSModel
+        A fiited GLM OLS model.
+    design : glmtools.design.DesignConfig
+        Design matrix object for GLM modelling.
+    glm_data : glmtools.data.TrialGLMData
+        Data object for GLM modelling.
+    """
+
+    # Validation
+    ndim = input_data.ndim
+    if ndim == 1:
+        raise ValueError("data must be 2D or greater.")
+
+    if dimension_labels is None:
+        dimension_labels = ["Subjects"] + [f"Features {i}" for i in range(1, ndim)]
+
+    # Load group information
+    SRC_DIR = "/ohba/pi/mwoolrich/scho/NTAD/src"
+    subject_ids, n_subjects = get_subject_ids(SRC_DIR, modality)
+    an_idx, ap_idx = load_group_information(subject_ids)
+    print(f"Number of available subjects: {n_subjects} | AN={len(an_idx)} | AP={len(ap_idx)}")
+    if n_subjects != (len(an_idx) + len(ap_idx)):
+        raise ValueError("one or more groups lacking subjects.")
+    
+    # Load meta data
+    df_meta = pd.read_excel(
+        "/home/scho/AnalyzeNTAD/scripts_data/all_data_info.xlsx"
+    )
+
+    # Define group assignments
+    group_assignments = np.zeros((n_subjects,))
+    group_assignments[ap_idx] = 1 # amyloid positive (w/ MCI & AD)
+    group_assignments[an_idx] = 2 # amyloid negative (controls)
+
+    # Define covariates (to regress out)
+    site_assignments = load_site_information(subject_ids)
+    scanner_assignments = load_scanner_information(subject_ids, df_meta, modality)
+    covariates = {
+        "site": site_assignments,
+        "scanner": scanner_assignments,
+    }
+
+    # Create GLM dataset
+    glm_data = glm.data.TrialGLMData(
+        data=input_data,
+        **covariates,
+        category_list=group_assignments,
+        dim_labels=dimension_labels,
+    )
+
+    # Create design matrix
+    DC = glm.design.DesignConfig()
+    DC.add_regressor(name="Group1", rtype="Categorical", codes=1) # amyloid positive
+    DC.add_regressor(name="Group2", rtype="Categorical", codes=2) # amyloid negative
+    for name in covariates:
+        DC.add_regressor(
+            name=name,
+            rtype="Parametric",
+            datainfo=name,
+            preproc=None,
+        )
+    DC.add_contrast(
+        name="GroupDiff",
+        values=[1, -1] + [0] * len(covariates)
+    ) # amyloid positive - amyloid negative
+    DC.add_contrast(
+        name="GroupMean",
+        values=[0.5, 0.5] + [0] * len(covariates)
+    )
+    design = DC.design_from_datainfo(glm_data.info)
+    if plot_verbose:
+        design.plot_summary(savepath=save_path)
+
+    # Fit GLM model
+    model = glm.fit.OLSModel(design, glm_data)
+
+    return model, design, glm_data
+
+def max_stat_perm_test(
+        glm_model,
+        glm_data,
+        design_matrix,
+        pooled_dims,
+        contrast_idx,
+        metric="tstats",
+        n_jobs=1
+    ):
+    """Perform statistical significance testing for the given contrast.
+
+    Parameters
+    ----------
+    glm_model : glmtools.fit.OLSModel
+        A fitted GLM OLS model.
+    glm_data : glmtools.data.TrialGLMData
+        Data object for GLM modelling.
+    design_matrix : glmtools.design.DesignConfig
+        Design matrix object for GLM modelling.
+    pooled_dims : int or tuples
+        Dimension(s) to pool over.
+    contrast_idx : int
+        Index indicating which contrast to use. Dependent on glm_model.
+    metric : str, optional
+        Metric to use to build the null distribution. Can be 'tstats' or 
+        'copes'.
+    n_jobs : int, optional
+        Number of processes to run in parallel.
+        
+
+    Returns
+    -------
+    pvalues : np.ndarray
+        P-values for the features. Shape is (n_features1, n_features2, ...).
+    """
+
+    # Run permutations and get null distributions
+    perm = glm.permutations.MaxStatPermutation(
+        design_matrix,
+        glm_data,
+        contrast_idx=contrast_idx,
+        nperms=10000,
+        metric=metric,
+        tail=0, # two-sided test
+        pooled_dims=pooled_dims,
+        nprocesses=n_jobs,
+    )
+    null_dist = perm.nulls
+
+    # Get p-values
+    if metric == "tstats":
+        print("Using tstats as metric")
+        tstats = abs(glm_model.tstats[0])
+        percentiles = stats.percentileofscore(null_dist, tstats)
+    elif metric == "copes":
+        print("Using copes as metric")
+        copes = abs(glm_model.copes[0])
+        percentiles = stats.percentileofscore(null_dist, copes)
+    pvalues = 1 - percentiles / 100
+
+    return pvalues
