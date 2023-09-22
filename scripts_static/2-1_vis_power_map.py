cb976ee5484c0e4b2ad15618b933eb8c74fe892a
Sungjun Cho <sungjun.cho@psych.ox.ac.uk>
2023-09-22 13:01:56 +0100

    fix: regress confounds out from power maps

--- scripts_static/2-1_vis_power_map.py
+++ scripts_static/2-1_vis_power_map.py
@@ -6,6 +6,7 @@
 import os
 import pickle
 import numpy as np
+from utils.statistics import fit_glm
 from utils.visualize import plot_group_power_map
 
 
@@ -15,7 +16,8 @@ if __name__ == "__main__":
     data_space = "source"
     freq_range = [1, 45]
     band_name = "wide"
-    print(f"[INFO] Modality: {modality.upper()} | Data Space: {data_space} | Frequency Band: {band_name} ({freq_range[0]}-{freq_range[1]} Hz)")
+    print(f"[INFO] Modality: {modality.upper()} | Data Space: {data_space} | " + 
+          "Frequency Band: {band_name} ({freq_range[0]}-{freq_range[1]} Hz)")
 
     # Set parcellation file paths
     mask_file = "MNI152_T1_8mm_brain.nii.gz"
@@ -31,15 +33,22 @@ if __name__ == "__main__":
     # Load data
     with open(os.path.join(DATA_DIR, "power.pkl"), "rb") as input_path:
         data = pickle.load(input_path)
-    power_an = data["power_map_an"]
-    power_ap = data["power_map_ap"]
-    print("Shape of AN power map: ", power_an.shape)
-    print("Shape of AP power map: ", power_ap.shape)
+    power_maps = data["power_maps"]
+    print("Shape of power maps: ", power_maps.shape)
 
-    # Calculate group-level power maps
-    gpower_an = np.mean(power_an, axis=0)
-    gpower_ap = np.mean(power_ap, axis=0)
-    gpower_diff = gpower_ap - gpower_an # amyloid positive vs. amyloid negative
+    # Fit GLM on power maps
+    power_model, power_design, power_data = fit_glm(
+        power_maps,
+        modality=modality,
+        dimension_labels=["Subjects", "Channels"],
+    )
+
+    # Get group-level power maps
+    gpower_an = power_model.betas[1]
+    gpower_ap = power_model.betas[0]
+    gpower_diff = gpower_ap - gpower_an # amyloid positive - amyloid negative
+    # dim: (n_channels,)
+    print("Shape of group-level power maps :", gpower_diff.shape)
 
     # Set visualization parameters
     hmax = np.max([gpower_an, gpower_ap])
